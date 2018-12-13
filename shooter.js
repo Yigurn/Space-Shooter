ffa9b467d915db5296941b4c91ae18d1bd322529
Joshua Lewis <45007621+Yigurn@users.noreply.github.com>
2018-12-13 16:33:25 +0000

    Add files via upload

--- shooter.js
+++ shooter.js
@@ -35,6 +35,39 @@ function setPoints() {
   document.cookie = "Points;" + points + ";path=/";
 }
 
+function setCookie(cname,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + points + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function checkCookie() {
+  var user=getCookie("cScore");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       //setCookie("username", user, 30);
+     }
+  }
+}
+
 var screen = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -69,7 +102,8 @@ function updateGameArea() {
         bullet[i].update();
     }
     score();
-    setPoints();
+    //setPoints();
+    setCookie("cScore", 10);
 }
 
 function Object(width, height, x, y, angle, colour, shape) {
